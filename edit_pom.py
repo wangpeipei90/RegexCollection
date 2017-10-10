501f5b9171ad854c6317ca4bb11d9aa63edca77e
wangpeipei90 <wangpeipei.90@gmail.com>
2017-10-10 17:03:18 -0400

    fix problem on no configuration or no argline

--- edit_pom.py
+++ edit_pom.py
@@ -11,18 +11,18 @@ xml_agent='''<!-- executes maven test with -javaagent option -->
                     <artifactId>maven-surefire-plugin</artifactId>
                     <version>2.14</version>
                     <configuration>
-                            <skipTests>false</skipTests>                            
+                            <skipTests>false</skipTests>
                             <argLine>-javaagent:target/${project.build.finalName}.jar=testUserLog</argLine>
                     </configuration>
             </plugin>'''
-            
+
 xml_javassist='''<dependency>
                         <groupId>org.javassist</groupId>
                         <artifactId>javassist</artifactId>
                         <version>3.14.0-GA</version>
                         <type>jar</type>
                 </dependency>'''
-                            
+
 
 #ET.register_namespace('', "http://www.opengis.net/wmts/1.0")
 #ET.register_namespace('ows', "http://www.opengis.net/ows/1.1")
@@ -36,7 +36,7 @@ def print_subtree(subtree):
 
 #for x in root:
 #    print x.tag, x.attrib
-#    print_subtree(x.getchildren())  
+#    print_subtree(x.getchildren())
 
 def prettify(elem):
     """Return a pretty-printed XML string for the Element.
@@ -51,7 +51,7 @@ def agent_pom(pom_file,proj_name):
     print "proj_name: ", proj_name
     tree = ET.parse(pom_file)
     root = tree.getroot()
-    
+
     #proj_name = root.find("name",namespaces=namespaces).text
     args_agent=arg_agent+str(proj_name)
 
@@ -62,38 +62,57 @@ def agent_pom(pom_file,proj_name):
     for plugin in plugins.findall(elem_prefix+"plugin"):
         artifactId_name=plugin.find(elem_prefix+"artifactId").text
         groupId_name=plugin.find(elem_prefix+"groupId").text
-        
+
         if artifactId_name=="maven-surefire-plugin" and groupId_name=="org.apache.maven.plugins":
-            args_JVM=plugin.find(elem_prefix+"configuration").find(elem_prefix+"argLine")
-            args_JVM.text=args_agent+" "+args_JVM.text
+            args_CONF=plugin.find(elem_prefix+"configuration")
+            if args_CONF is not None:
+                args_JVM=args_CONF.find(elem_prefix+"argLine")
+                if args_JVM is not None:
+                    args_JVM.text=args_agent+" "+args_JVM.text
+                else:##argLine is missing
+                    args_JVM=ET.SubElement(args_CONF,"argLine")
+                    args_JVM.text=args_agent
+            else: ##argCONF is missing
+                 args_CONF=ET.SubElement(plugin_surefire,"configuration")
+                 args_JVM=ET.SubElement(args_CONF,"argLine")
+                 args_JVM.text=args_agent
             isfound_javaagent=True
             break
-    
     if not isfound_javaagent:
-        xml_agent.replace("testUserLog",str(proj_name))
-        elem_agent=ET.fromstring(xml_agent)
-        plugins.insert(len(plugins),elem_agent)
-        
+        plugin_surefire=ET.fromstring(xml_agent)
+        plugins.insert(len(plugins),plugin_surefire)
+        args_JVM=plugin_surefire.find("configuration").find("argLine")
+        args_JVM.text=args_agent
+        # <plugin>
+        #             <groupId>org.apache.maven.plugins</groupId>
+        #             <artifactId>maven-surefire-plugin</artifactId>
+        #             <version>2.14</version>
+        #             <configuration>
+        #                     <skipTests>false</skipTests>
+        #                     <argLine>-javaagent:target/${project.build.finalName}.jar=testUserLog</argLine>
+        #             </configuration>
+        #     </plugin>'''
+
     deps=root.find(elem_prefix+"dependencies",namespaces=namespaces)
     isfound_javaassist=False
     for dep in deps.findall(elem_prefix+"dependency"):
         artifactId_name=dep.find(elem_prefix+"artifactId").text
         groupId_name=dep.find(elem_prefix+"groupId").text
-        
+
         if artifactId_name=="javassist" and groupId_name=="org.javassist":
             isfound_javaassist=True
             break
-    
+
     if not isfound_javaassist:
-        elem_javassist=ET.fromstring(xml_javassist)  
+        elem_javassist=ET.fromstring(xml_javassist)
         deps.insert(len(deps),elem_javassist)
-        
-    #ET.dump(root)    
+
+    #ET.dump(root)
     rough_string = ET.tostring(root,'utf-8')
     #rough_string = ET.tostring(elem)
     #reparsed = minidom.parseString(rough_string.encode('utf-8').decode('utf-8'))
    # print reparsed.toprettyxml(indent="  ")
-    file = open("pom.xml", 'wb')
+    file = open("pom2.xml", 'wb')
     file.write(rough_string)
     file.close()
 #end
